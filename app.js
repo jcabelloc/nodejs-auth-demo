469ca9e500be19dd2fda3b6b752580c17e35e87c
Juan Cabello <jcabelloc@gmail.com>
2018-03-26 23:01:54 -0500

    Adding Logout and isLoggedIn Middleware

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get("/", function(req, res){
     res.render("home");
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
     res.render("secret");
 });
 
@@ -69,6 +69,18 @@ app.post("/login", passport.authenticate("local", {
 }), function(req, res){
 
 });
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/")
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+};
 
 app.listen(3000, function(){
     console.log("server started...");
