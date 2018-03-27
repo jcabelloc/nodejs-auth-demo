360e64d401e86db214d41fd725f79df70c628cc2
Juan Cabello <jcabelloc@gmail.com>
2018-03-26 22:03:47 -0500

    Register Routes and Form

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
 app.set("view engine", "ejs");
-
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(require("express-session")({
     secret: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet",
     resave: false,
@@ -23,6 +23,10 @@ app.use(passport.session());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+//=======================
+// ROUTES
+//=======================
+
 app.get("/", function(req, res){
     res.render("home");
 });
@@ -30,6 +34,28 @@ app.get("/", function(req, res){
 app.get("/secret", function(req, res){
     res.render("secret");
 });
+
+// Auth Routes
+// show sign up form
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+// handling user sign up
+app.post("/register", function(req, res){
+
+    User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            return res.render("register");
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret");
+        });
+    });
+});
+
+
 
 app.listen(3000, function(){
     console.log("server started...");
